e143e661bb699df452df791ecead346e89cab150
Adrian Apodaca <aapodaca@gmail.com>
2020-10-10 17:06:38 -0700

    remove typo in game view

--- public/bundle.js
+++ public/bundle.js
@@ -604,7 +604,28 @@ class Game {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module parse failed: Unexpected token (19:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|     // bindKeyHandlers() {\n|     // }\n> }\n| \n| ");
+class GameView {
+    constructor(ctx, game) {
+        this.ctx = ctx;
+        this.game = game;
+
+    }
+
+    start() {
+        requestAnimationFrame(this.animate.bind(this))
+    }
+    
+    animate () {
+        this.game.draw(this.ctx);
+        requestAnimationFrame(this.animate.bind(this))
+    }
+  
+    // bindKeyHandlers() {
+    // }
+}
+
+
+module.exports = GameView;
 
 /***/ }),
 
